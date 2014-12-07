c8b73c56f98b13953eea91844afbe1092bdaa136
adikus <andrej.hoos@gmail.com>
2014-12-07 07:15:07 +0000

    add track name

--- player.py
+++ player.py
@@ -5,10 +5,24 @@ class Player:
 		self.y = 0
 		self.r = 0
 
+		self.fx = 0
+		self.fy = 0
+		self.fr = 0
+
+		self.bx = 0
+		self.by = 0
+		self.br = 0
+
 	def set_position(self, position):
 		self.x = position['x']
 		self.y = position['y']
 		self.r = position['r']
 
+		self.fx = position['fx']
+		self.fy = position['fy']
+
+		self.bx = position['bx']
+		self.by = position['by']
+
 	def get_data(self):
-		return {"x": self.x, "y": self.y, "r": self.r}
+		return {"x": self.x, "y": self.y, "r": self.r, "fx": self.fx, "fy": self.fy, "bx": self.bx, "by": self.by}
